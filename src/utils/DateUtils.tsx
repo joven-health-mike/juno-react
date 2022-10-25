b2d96feb82d8ad4eeef1be798e48434c3874091c
Mike Burke <mike@jovenhealth.com>
2022-10-25 19:24:46 -0400

    Refactor DateUtil functions to use Luxon library

--- src/utils/DateUtils.tsx
+++ src/utils/DateUtils.tsx
@@ -23,22 +23,14 @@ export function formatDate(date: Date, timeZone: string | undefined): string {
   const luxonDate = DateTime.fromJSDate(date).setZone(
     timeZone || DEFAULT_TIME_ZONE
   );
-  return `${luxonDate.month}/${luxonDate.day}`;
+  return luxonDate.toFormat('M/dd');
 }
 
 export function formatTime(date: Date, timeZone: string | undefined): string {
   const luxonDate = DateTime.fromJSDate(date).setZone(
     timeZone || DEFAULT_TIME_ZONE
   );
-  let hourStr = `${luxonDate.hour}`;
-  let amPmStr = `am`;
-  if (luxonDate.hour === 0 || luxonDate.hour === 12) {
-    hourStr = `12`;
-  } else if (luxonDate.hour > 12) {
-    hourStr = `${luxonDate.hour - 12}`;
-    amPmStr = `pm`;
-  }
-  return `${hourStr}:${luxonDate.minute} ${amPmStr}`;
+  return luxonDate.toFormat('h:mm a');
 }
 
 export function formatDateTime(
