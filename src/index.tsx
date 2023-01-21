4d46a6c15df4e45f51b7492035a857e403c340f8
Zoë Gonzales <38014614+zoe-gonzales@users.noreply.github.com>
2023-01-21 08:55:39 -0700

    fix import of main.css

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
 // Copyright 2022 Social Fabric, LLC
-import React from "react"
-import { createRoot } from "react-dom/client"
-import { BrowserRouter } from "react-router-dom"
-import App from "./App"
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import './assets/main.css';
 
-const container = document.getElementById("root")
-const root = createRoot(container!)
+const container = document.getElementById('root');
+const root = createRoot(container!);
 
 root.render(
   <React.StrictMode>
@@ -13,4 +14,4 @@ root.render(
       <App />
     </BrowserRouter>
   </React.StrictMode>
-)
+);
