7f4086a63e2054c42f04e782f0ee4e05a037bed5
Zoë Gonzales <38014614+zoe-gonzales@users.noreply.github.com>
2023-02-06 22:43:02 -0700

    migrate .modal

--- src/components/modals/BaseModal.tsx
+++ src/components/modals/BaseModal.tsx
@@ -2,6 +2,24 @@
 
 import React from 'react';
 import Modal from 'react-modal';
+import styled from 'styled-components';
+
+const StyledModal = styled(Modal)`
+  position: absolute;
+  max-height: 80vh;
+  float: left;
+  width: 500px;
+  max-height: 80vh;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  background-color: #f6f740;
+  border: 2px solid darkslateblue;
+  border-radius: 25px;
+  padding: 50px 150px;
+  box-shadow: 5px 5px 10px 5px #385aa8;
+  overflow: auto;
+`;
 
 type ModalProps = {
   isOpen: boolean;
@@ -15,14 +33,13 @@ const BaseModal: React.FC<ModalProps> = ({
   children,
 }) => {
   return (
-    <Modal
+    <StyledModal
       isOpen={isOpen}
       onRequestClose={onRequestClose}
-      className={'modal'}
       overlayClassName={'overlay'}
     >
       {children}
-    </Modal>
+    </StyledModal>
   );
 };
 
