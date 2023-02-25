44d4ede145a63d2750be9d38bdbeb4e7e61f47ad
Zoë Gonzales <38014614+zoe-gonzales@users.noreply.github.com>
2023-02-25 12:28:48 -0700

    XButton test

--- src/components/buttons/XButton.test.tsx
+++ src/components/buttons/XButton.test.tsx
@@ -3,26 +3,25 @@
  */
 
 import React from 'react';
-import { fireEvent, render } from '@testing-library/react';
+
+import { fireEvent, render, screen } from '@testing-library/react';
 import XButton from './XButton';
 
 describe('XButton', () => {
-  it('should call the onClick function when the button is clicked', async () => {
+  it('should call the onClick function when the button is clicked', () => {
     const value = 'Hello World';
     const callback = jest.fn();
-    const view = render(<XButton value={value} onClick={callback} />);
-    // eslint-disable-next-line testing-library/prefer-screen-queries
-    const button = await view.findByTestId('button-x');
+    render(<XButton value={value} onClick={callback} />);
+    const button = screen.getByRole('button');
     fireEvent.click(button);
     expect(callback).toHaveBeenCalled();
   });
 
-  it('should set the value to the button', async () => {
+  it('should set the value to the button', () => {
     const value = 'Hello World';
     const callback = jest.fn();
-    const view = render(<XButton value={value} onClick={callback} />);
-    // eslint-disable-next-line testing-library/prefer-screen-queries
-    const button = (await view.findByTestId('button-x')) as HTMLButtonElement;
+    render(<XButton value={value} onClick={callback} />);
+    const button = screen.getByRole('button') as HTMLButtonElement;
     expect(button.value).toEqual(value);
   });
 });
