029688f55bf9b1be4c6e677785e703b08fd4a1b2
Zoë Gonzales <38014614+zoe-gonzales@users.noreply.github.com>
2023-02-25 13:09:21 -0700

    UserDetails test

--- src/components/details/UserDetails.test.tsx
+++ src/components/details/UserDetails.test.tsx
@@ -4,7 +4,7 @@
 /* eslint-disable testing-library/prefer-screen-queries */
 
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import UserDetails from './UserDetails';
 import { Role } from '../../services/user.service';
 
@@ -20,37 +20,24 @@ const testUser = {
 };
 
 describe('UserDetails', () => {
-  it('should display name', async () => {
-    const view = render(<UserDetails user={testUser} />);
-    const nameView = await view.findByTestId('name');
-    expect(nameView.innerHTML).toEqual(
-      testUser.firstName + ' ' + testUser.lastName
+  const renderComponent = () => render(<UserDetails user={testUser} />);
+
+  it('should display name', () => {
+    renderComponent();
+    const nameView = screen.getByText(
+      `${testUser.firstName} ${testUser.lastName}`
     );
+    expect(nameView).not.toBeNull();
   });
-  it('should display email as a string', async () => {
-    const view = render(<UserDetails user={testUser} />);
-    const emailView = await view.findByTestId('email');
-    expect(emailView.innerHTML).toEqual('Email: ' + testUser.email.toString());
+  it('should display email as a string', () => {
+    renderComponent();
+    const emailView = screen.getByText(`Email: ${testUser.email.toString()}`);
+    expect(emailView).not.toBeNull();
   });
 
-  it('should display the user role', async () => {
-    const view = render(<UserDetails user={testUser} />);
-    const userRoleView = await view.findByTestId('userRole');
-    expect(userRoleView.innerHTML).toEqual('Role: ' + testUser.role.toString());
-  });
-  it('should display name as <h2>', async () => {
-    const view = render(<UserDetails user={testUser} />);
-    const nameView = await view.findByTestId('name');
-    expect(nameView.nodeName.toLowerCase()).toEqual('h2');
-  });
-  it('should display email as <p>', async () => {
-    const view = render(<UserDetails user={testUser} />);
-    const emailView = await view.findByTestId('email');
-    expect(emailView.nodeName.toLowerCase()).toEqual('p');
-  });
-  it('should display the role as <p>', async () => {
-    const view = render(<UserDetails user={testUser} />);
-    const roleView = await view.findByTestId('userRole');
-    expect(roleView.nodeName.toLowerCase()).toEqual('p');
+  it('should display the user role', () => {
+    renderComponent();
+    const userRoleView = screen.getByText(`Role: ${testUser.role.toString()}`);
+    expect(userRoleView).not.toBeNull();
   });
 });
--- src/components/details/UserDetails.tsx
+++ src/components/details/UserDetails.tsx
@@ -16,15 +16,15 @@ type UserDetailsProps = {
 const UserDetails: React.FC<UserDetailsProps> = ({ user }) => {
   return (
     <>
-      <h2 data-testid={'name'}>{`${user.firstName} ${user.lastName}`}</h2>
-      <p data-testid={'email'}>Email: {user.email}</p>
+      <h2>{`${user.firstName} ${user.lastName}`}</h2>
+      <p>Email: {user.email}</p>
       <p data-testid={'username'}>Username: {user.username}</p>
       <p data-testid={'phone'}>Phone: {user.phone}</p>
       <p data-testid={'docsUrl'}>
         Docs URL: <Link href={user.docsUrl}>{user.docsUrl}</Link>
       </p>
       <p data-testid={'timeZoneIanaName'}>Time Zone: {user.timeZoneIanaName}</p>
-      <p data-testid={'userRole'}>Role: {user.role}</p>
+      <p>Role: {user.role}</p>
     </>
   );
 };
