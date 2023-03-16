86d21f05bfffc979c19600139918a3268695b0d4
Mike Burke <mike@jovenhealth.com>
2023-03-16 15:16:18 -0600

    Add form validation to StudentDialog

--- src/components/modals/StudentDialog.tsx
+++ src/components/modals/StudentDialog.tsx
@@ -12,8 +12,18 @@ import {
 import React, { useContext, useEffect, useMemo, useState } from 'react';
 import { getCounselors } from '../../data/counselors';
 import { SchoolsContext } from '../../data/schools';
-import { Student, StudentStatus, STUDENT_STATUSES } from '../../data/students';
+import {
+  emptyStudent,
+  Student,
+  StudentStatus,
+  STUDENT_STATUSES,
+} from '../../data/students';
 import { UsersContext } from '../../data/users';
+import {
+  isValidEmail,
+  isValidPhoneNumber,
+  isValidURL,
+} from '../../services/http-common';
 import { TIME_ZONES } from '../../utils/DateUtils';
 import MaterialDialog from './MaterialDialog';
 
@@ -38,18 +48,102 @@ const StudentDialog: React.FC<StudentDialogProps> = ({
   const { data: users } = useContext(UsersContext);
   const { data: schools } = useContext(SchoolsContext);
   const counselors = useMemo(() => getCounselors(users), [users]);
+  const [firstNameError, setFirstNameError] = useState(false);
+  const [lastNameError, setLastNameError] = useState(false);
+  const [emailError, setEmailError] = useState(false);
+  const [usernameError, setUsernameError] = useState(false);
+  const [phoneError, setPhoneError] = useState(false);
+  const [docsUrlError, setDocsUrlError] = useState(false);
+  const [timeZoneError, setTimeZoneError] = useState(false);
+  const [statusError, setStatusError] = useState(false);
+  const [counselorError, setCounselorError] = useState(false);
+  const [schoolError, setSchoolError] = useState(false);
 
   useEffect(() => {
     setStudent({ ...initialStudent });
   }, [initialStudent]);
 
+  const validateInputs = () => {
+    let allInputsValid = true;
+
+    if (student.firstName.length === 0) {
+      setFirstNameError(true);
+      allInputsValid = false;
+    } else setFirstNameError(false);
+
+    if (student.lastName.length === 0) {
+      setLastNameError(true);
+      allInputsValid = false;
+    } else setLastNameError(false);
+
+    if (student.email?.length! > 0 && !isValidEmail(student.email!)) {
+      setEmailError(true);
+      allInputsValid = false;
+    } else setEmailError(false);
+
+    if (
+      typeof student.username === 'undefined' ||
+      student.username.length === 0 ||
+      student.username.length > 15
+    ) {
+      setUsernameError(true);
+      allInputsValid = false;
+    } else setUsernameError(false);
+
+    if (
+      typeof student.docsUrl === 'undefined' ||
+      student.docsUrl.length === 0 ||
+      !isValidURL(student.docsUrl)
+    ) {
+      setDocsUrlError(true);
+      allInputsValid = false;
+    } else setDocsUrlError(false);
+
+    if (student.phone?.length! > 0 && !isValidPhoneNumber(student.phone!)) {
+      setPhoneError(true);
+      allInputsValid = false;
+    } else setPhoneError(false);
+
+    if (
+      typeof student.studentAssignedSchoolId === 'undefined' ||
+      student.studentAssignedSchoolId.length === 0
+    ) {
+      setSchoolError(true);
+      allInputsValid = false;
+    } else setSchoolError(false);
+
+    if (TIME_ZONES.indexOf(`${student.timeZoneIanaName}`) === -1) {
+      setTimeZoneError(true);
+      allInputsValid = false;
+    } else setTimeZoneError(false);
+
+    return allInputsValid;
+  };
+
   const onFormSubmit = () => {
+    const validInputs = validateInputs();
+    if (!validInputs) return;
+
     const submittedStudent = { ...student };
     submittedStudent.role = isTeacher ? 'TEACHER' : 'STUDENT';
     onStudentAdded(submittedStudent);
+
+    setStudent(emptyStudent);
     onClose();
   };
+
   const onFormCancel = () => {
+    setStudent(emptyStudent);
+    setFirstNameError(false);
+    setLastNameError(false);
+    setEmailError(false);
+    setUsernameError(false);
+    setPhoneError(false);
+    setDocsUrlError(false);
+    setTimeZoneError(false);
+    setStatusError(false);
+    setCounselorError(false);
+    setSchoolError(false);
     onClose();
   };
 
@@ -58,68 +152,94 @@ const StudentDialog: React.FC<StudentDialogProps> = ({
       <MaterialDialog open={isOpen} onClose={onClose}>
         <DialogTitle>{title}</DialogTitle>
         <DialogContent>
-          <FormControl fullWidth sx={{ mt: 2, mb: 2 }}>
-            <InputLabel id="firstName">First Name</InputLabel>
+          <FormControl fullWidth required sx={{ mt: 2, mb: 2 }}>
+            <InputLabel id="firstName" error={firstNameError}>
+              First Name
+            </InputLabel>
             <Input
               id="firstName"
               value={student.firstName}
               onChange={e => {
+                e.preventDefault();
+                setFirstNameError(false);
                 setStudent({ ...student, firstName: e.target.value });
               }}
             />
           </FormControl>
-          <FormControl fullWidth sx={{ mb: 2 }}>
-            <InputLabel id="lastName">Last Name</InputLabel>
+          <FormControl fullWidth required sx={{ mb: 2 }}>
+            <InputLabel id="lastName" error={lastNameError}>
+              Last Name
+            </InputLabel>
             <Input
               id="lastName"
               value={student.lastName}
               onChange={e => {
+                e.preventDefault();
+                setLastNameError(false);
                 setStudent({ ...student, lastName: e.target.value });
               }}
             />
           </FormControl>
           <FormControl fullWidth sx={{ mb: 2 }}>
-            <InputLabel htmlFor="email">Email address</InputLabel>
+            <InputLabel htmlFor="email" error={emailError}>
+              Email address
+            </InputLabel>
             <Input
               id="email"
               value={student.email}
               onChange={e => {
+                e.preventDefault();
+                setEmailError(false);
                 setStudent({ ...student, email: e.target.value });
               }}
             />
           </FormControl>
-          <FormControl fullWidth sx={{ mb: 2 }}>
-            <InputLabel id="username">Username</InputLabel>
+          <FormControl fullWidth required sx={{ mb: 2 }}>
+            <InputLabel id="username" error={usernameError}>
+              Username
+            </InputLabel>
             <Input
               id="username"
               value={student.username}
               onChange={e => {
+                e.preventDefault();
+                setUsernameError(false);
                 setStudent({ ...student, username: e.target.value });
               }}
             />
           </FormControl>
           <FormControl fullWidth sx={{ mb: 2 }}>
-            <InputLabel id="phone">Phone number</InputLabel>
+            <InputLabel id="phone" error={phoneError}>
+              Phone number
+            </InputLabel>
             <Input
               id="phone"
               value={student.phone}
               onChange={e => {
+                e.preventDefault();
+                setPhoneError(false);
                 setStudent({ ...student, phone: e.target.value });
               }}
             />
           </FormControl>
-          <FormControl fullWidth sx={{ mb: 2 }}>
-            <InputLabel id="docsUrl">Docs URL</InputLabel>
+          <FormControl fullWidth required sx={{ mb: 2 }}>
+            <InputLabel id="docsUrl" error={docsUrlError}>
+              Docs URL
+            </InputLabel>
             <Input
               id="docsUrl"
               value={student.docsUrl}
               onChange={e => {
+                e.preventDefault();
+                setDocsUrlError(false);
                 setStudent({ ...student, docsUrl: e.target.value });
               }}
             />
           </FormControl>
           <FormControl fullWidth sx={{ mb: 2 }}>
-            <InputLabel id="counselor">Counselor</InputLabel>
+            <InputLabel id="counselor" error={counselorError}>
+              Counselor
+            </InputLabel>
             <Select
               labelId="counselor"
               id="counselor"
@@ -127,6 +247,8 @@ const StudentDialog: React.FC<StudentDialogProps> = ({
               value={student.studentAssignedCounselorId}
               label="Counselor"
               onChange={e => {
+                e.preventDefault();
+                setCounselorError(false);
                 setStudent({
                   ...student,
                   studentAssignedCounselorId: e.target.value,
@@ -143,8 +265,10 @@ const StudentDialog: React.FC<StudentDialogProps> = ({
               })}
             </Select>
           </FormControl>
-          <FormControl fullWidth sx={{ mb: 2 }}>
-            <InputLabel id="school">School</InputLabel>
+          <FormControl fullWidth required sx={{ mb: 2 }}>
+            <InputLabel id="school" error={schoolError}>
+              School
+            </InputLabel>
             <Select
               labelId="school"
               id="school"
@@ -152,6 +276,8 @@ const StudentDialog: React.FC<StudentDialogProps> = ({
               value={student.studentAssignedSchoolId}
               label="School"
               onChange={e => {
+                e.preventDefault();
+                setSchoolError(false);
                 setStudent({
                   ...student,
                   studentAssignedSchoolId: e.target.value,
@@ -167,20 +293,24 @@ const StudentDialog: React.FC<StudentDialogProps> = ({
               })}
             </Select>
           </FormControl>
-          <FormControl fullWidth sx={{ mb: 2 }}>
-            <InputLabel id="timeZone">Time Zone</InputLabel>
+          <FormControl fullWidth required sx={{ mb: 2 }}>
+            <InputLabel id="timeZone" error={timeZoneError}>
+              Time Zone
+            </InputLabel>
             <Select
               labelId="timeZone"
               id="timeZone"
               defaultValue={TIME_ZONES[0]}
               value={student.timeZoneIanaName}
               label="Time Zone"
-              onChange={e =>
+              onChange={e => {
+                e.preventDefault();
+                setTimeZoneError(false);
                 setStudent({
                   ...student,
                   timeZoneIanaName: e.target.value,
-                })
-              }
+                });
+              }}
             >
               {TIME_ZONES.map((timeZone, index) => (
                 <MenuItem value={timeZone} key={index}>
@@ -189,20 +319,24 @@ const StudentDialog: React.FC<StudentDialogProps> = ({
               ))}
             </Select>
           </FormControl>
-          <FormControl fullWidth sx={{ mb: 2 }}>
-            <InputLabel id="status">Status</InputLabel>
+          <FormControl fullWidth required sx={{ mb: 2 }}>
+            <InputLabel id="status" error={statusError}>
+              Status
+            </InputLabel>
             <Select
               labelId="status"
               id="status"
               defaultValue={STUDENT_STATUSES[0]}
               value={student.studentStatus}
               label="Status"
-              onChange={e =>
+              onChange={e => {
+                e.preventDefault();
+                setStatusError(false);
                 setStudent({
                   ...student,
                   studentStatus: e.target.value as StudentStatus,
-                })
-              }
+                });
+              }}
             >
               {STUDENT_STATUSES.map((status, index) => (
                 <MenuItem value={status} key={index}>
